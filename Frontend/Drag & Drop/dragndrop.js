b9db4e1692777af8401a86e867a6cb782a490ded
피자 가게 <128763219+Jinwoooooooo@users.noreply.github.com>
2023-12-05 15:08:18 +0900

    12.05
    1학년 2학기 프론트엔드II 수업

--- Frontend/Drag & Drop/dragndrop.js
+++ Frontend/Drag & Drop/dragndrop.js
@@ -7,3 +7,72 @@
 //* ondragover - 드래깅 객체가 영역 위로 왔을 때
 //* ondragleave - 드래깅 객체가 영역 밖으로 나갔을 때
 //* ondrop - 드래깅 객체를 영역 위에 놓았을 때.
+
+//!======================================================
+//* Global Variable Zone
+
+let draggingCard = null; //? 현재 드래깅 중인 카드 객체
+let dragOverBox = null; //? 드래깅 중인 카드 객체가 올라가 있는 박스
+let dragOverCard = null; //? 드래깅 중인 카드 객체가 올라가 있는 카드
+//!======================================================
+//* Card Event Handler
+
+function onDragStartCard(event) {
+    draggingCard = this; //? 드래깅 중인 카드를 드래깅 중인 카드로 설정
+    this.classList.add("draggingCard");
+}
+function onDragEndCard(event) {
+    draggingCard = null; //? 드래깅 중인 카드가 없으므로 설정.
+    this.classList.remove("draggingCard");
+
+    //? dragOver 상태에서 드래깅이 종료되면 dragLeave 이벤트가 발생하지 않고
+    //? 종료되었기 때문에 box 배경색이 복구되지 않는다. 그러므로 추가 정리가 필요.
+    if(dragOverBox != null) {
+        dragOverBox.classList.remove("overBox");
+        dragOverBox = null;
+    }
+}
+function onDragOverCard(event) {
+    event.preventDefault(); //? 웹 브라우저의 기본 동작을 막는 메소드
+    dragOverCard = this;
+    this.classList.add("overCard");
+}
+function onDragLeaveCard(event) {
+    this.classList.remove("overCard");
+    dragOverCard = null;
+}
+//!======================================================
+//* Box Event Handler
+
+function onDragOverBox(event) {
+    event.preventDefault(); //? 웹 브라우저의 기본 동작을 막는 메소드
+    dragOverBox = this;
+    this.classList.add("overBox");
+}
+function onDragLeaveBox(event) {
+    dragOverBox = null;
+    this.classList.remove("overBox");
+}
+function onDropBox(event) {
+    event.preventDefault();
+    this.appendChild(draggingCard); 
+    //? Drop 이벤트가 발생한 box 객체(this)의 자식 노드로 드래깅 중인 객체를 연결한다.
+}
+//!======================================================
+$(document).ready(function() {
+    let cardArray = document.getElementsByClassName("card");
+    for(let card of cardArray) {
+        card.addEventListener("dragstart", onDragStartCard);
+        card.addEventListener("dragend", onDragEndCard);
+        card.addEventListener("dragover", onDragOverCard);
+        card.addEventListener("dragleave", onDragLeaveCard);
+    }
+//!======================================================
+    let boxArray = document.getElementsByClassName("box");
+    for(let box of boxArray) {
+        box.addEventListener("dragover", onDragOverBox);
+        box.addEventListener("dragleave", onDragLeaveBox);
+        box.addEventListener("drop", onDropBox);
+    }
+})
+//!======================================================
